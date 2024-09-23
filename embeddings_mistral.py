9e75bb67f80b6afca1ccf55c8040bc4d3f36e7d3
Laure-di <masson.laure.l@gmail.com>
2024-09-23 18:05:10 +0200

    feature: manage new object in bucket

--- embeddings_mistral.py
+++ embeddings_mistral.py
@@ -3,20 +3,22 @@ import os
 
 
 import psycopg2
+from langchain_postgres import PGVector
 from scaleway import Client
 from scaleway.inference.v1beta1.api import InferenceV1Beta1API
 from scaleway.inference.v1beta1.types import EndpointSpec
 from scaleway.inference.v1beta1.types import EndpointSpecPublic
 from scaleway.rdb.v1.api import RdbV1API
+from scaleway.rdb.v1.types import Permission
 from dotenv import load_dotenv
-from langchain_community.document_loaders import S3DirectoryLoader
+from langchain_community.document_loaders import S3DirectoryLoader, S3FileLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_openai import OpenAIEmbeddings
-
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chains import RetrievalQA
 
 MODEL_NAME = "mistral/mistral-7b-instruct-v0.3:bf16"
 NODE_TYPE = "L4"
-BUCKET_NAME = "cli-test"
+BUCKET_NAME = "rag-test"
 DEPLOYMENT_NAME = "RAG"
 INSTANCE_NAME = "RAG-instance"
 DB_NAME = "DB_RAG"
@@ -86,22 +88,24 @@ if __name__ == "__main__":
 
     exist = deployment_exists(DEPLOYMENT_NAME_EMBED)
     if not exist:
-        deployment = inference_api.create_deployment(model_name=MODEL_NAME_EMBED, node_type=NODE_TYPE, endpoints=[endpoint],
+        deployment_embeddings = inference_api.create_deployment(model_name=MODEL_NAME_EMBED, node_type=NODE_TYPE, endpoints=[endpoint],
                                                      project_id=project_id, name=DEPLOYMENT_NAME_EMBED)
     else:
-        deployment = deployment_by_name(DEPLOYMENT_NAME_EMBED)
+        deployment_embeddings = deployment_by_name(DEPLOYMENT_NAME_EMBED)
 
-
+    logger.debug("Create or get DB")
     instance_exist = instance_exists()
     if not instance_exist:
         instance_db = db_api.create_instance(engine="PostgreSQL-15", user_name="laure-di", password=os.getenv("SCW_DB_PASSWORD"), node_type="db-play2-nano", region="fr-par", is_ha_cluster=False,disable_backup=True, volume_size=10000000000, volume_type="bssd", backup_same_region=False, name=INSTANCE_NAME)
-        db_api.wait_for_instance(instance_id=instance_db.id)
+        instance_db = db_api.wait_for_instance(instance_id=instance_db.id)
         db = db_api.create_database(instance_id=instance_db.id, name=DB_NAME)
-
+        db_api.set_privilege(instance_id=instance_db.id, database_name=DB_NAME, user_name="laure-di", permission=Permission.ALL)
     else:
-        instance_db = instance_by_name(INSTANCE_NAME)
+        instance_db = instance_by_name()
         db = db_by_instance(instance_db.id)
-    logger.debug("start trying to connect to database")
+
+
+    logger.debug("Start trying to connect to database")
     conn = psycopg2.connect(
         database=DB_NAME,
         user=DB_USER,
@@ -110,31 +114,81 @@ if __name__ == "__main__":
         port=instance_db.endpoint.port  # Use the correct port
     )
     cur = conn.cursor()
-    logger.debug("connected to database")
-    cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS document_vectors (
-            id SERIAL PRIMARY KEY,
-            document_id TEXT,
-            content TEXT,
-            vector VECTOR(1536)  -- Adjust the dimension based on your embeddings
-        );
-    """)
-    logger.debug("finish request")
-    conn.commit()
-    logger.debug("start loading documents")
-    endpoint_s3 = "https://s3." + os.getenv("SCW_DEFAULT_REGION", "") + ".scw.cloud"
-    document = S3DirectoryLoader(bucket=BUCKET_NAME, endpoint_url=endpoint_s3,
-                                 aws_access_key_id=os.getenv("SCW_ACCESS_KEY", ""),
-                                 aws_secret_access_key=os.getenv("SCW_SECRET_KEY", ""))
-    files = document.load()
-    logger.debug("finished loading documents")
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    logger.debug("splitting in chunks")
-    chunks = text_splitter.split_documents(files)
-    embeddings = OpenAIEmbeddings(
-        openai_api_key=os.getenv("SCW_API_KEY"),
-        openai_api_base=os.getenv("ENDPOINT"),
-        model="mistral-7b-instruct-v0.3")
-    embed = embeddings.embed_documents([chunk.page_content for chunk in chunks])
+    logger.debug("Connected to database")
 
+    logger.debug("Install pg_vector extension")
+    cur.execute("CREATE EXTENSION IF NOT EXISTS vector;")
+
+
+    logger.debug("Create table object_key")
+    cur.execute("""CREATE TABLE IF NOT EXISTS object_loaded (id SERIAL PRIMARY KEY, object_key TEXT)""")
+
+
+    endpoint_s3 = "https://s3." + os.getenv("SCW_DEFAULT_REGION", "") + ".scw.cloud"
+    documentLoader = S3DirectoryLoader(bucket=BUCKET_NAME, endpoint_url=endpoint_s3,
+                                       aws_access_key_id=os.getenv("SCW_ACCESS_KEY", ""),
+                                       aws_secret_access_key=os.getenv("SCW_SECRET_KEY", ""))
+
+
+    embedding_url = deployment_embeddings.endpoints[0].url + "/v1"
+    embeddings = OpenAIEmbeddings(
+        openai_api_key=os.getenv("SCW_API_KEY_EMBED"),
+        openai_api_base=embedding_url,
+        model="sentence-transformers/sentence-t5-xxl",
+        tiktoken_enabled=False,
+    )
+
+    os.environ['TOKENIZERS_PARALLELISM'] = 'false'
+    connection_string = "postgresql+psycopg2://" + conn.info.user + ":" + conn.info.password + "@" + conn.info.host + ":" + str(
+        conn.info.port) + "/" + conn.info.dbname
+    vector_store = PGVector(
+        connection=connection_string,
+        embeddings=embeddings,
+    )
+
+    logger.debug("start loading documents lazy way")
+    files = documentLoader.lazy_load()
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=300, chunk_overlap=20)
+    for file in files:
+        cur.execute("SELECT object_key FROM object_loaded WHERE object_key = %s", (file.metadata["source"],))
+        if cur.fetchone() is None:
+            logger.debug("start loading files because it doesn't exist in db")
+            fileLoader = S3FileLoader(bucket=BUCKET_NAME, key=file.metadata["source"] ,endpoint_url=endpoint_s3,
+                                      aws_access_key_id=os.getenv("SCW_ACCESS_KEY", ""),
+                                      aws_secret_access_key=os.getenv("SCW_SECRET_KEY", ""))
+            file_to_load = fileLoader.load()
+            cur.execute("INSERT INTO object_loaded (object_key) VALUES (%s)", (file.metadata["source"],))
+            chunks = text_splitter.split_text(file.page_content)
+            try:
+                logger.debug("start embedding chunks")
+                embeddings_list = [embeddings.embed_query(chunk) for chunk in chunks]
+                for embeddings, chunk in embeddings_list, chunks:
+                    logger.debug("add embeddings to db")
+                    vector_store.add_embeddings(embeddings, chunk)
+            except Exception as e:
+                logging.error(f"An error occurred: {e}")
+
+    conn.commit()
+
+    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+
+    deployment_url = deployment.endpoints[0].url + "/v1"
+    llm = ChatOpenAI(
+        base_url=deployment_url,
+        api_key=os.getenv("SCW_API_KEY"),
+        model=deployment.model_name,
+        temperature=0.0,
+    )
+
+    qa_stuff = RetrievalQA.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever,
+        verbose=True,
+    )
+
+    query = "What are the step to setup a database with the cli of scaleway?"
+
+    response = qa_stuff.invoke(query)
+
+    print(response['result'])
